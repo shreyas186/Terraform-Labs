2c4725ce2b4285b3c27c3530e036cb531df7c196
shreyas186 <shreyas.acharya.186@gmail.com>
2024-01-20 14:32:51 +0530

    Changed to randomid

--- Terraform-Labs/storage-account.tf
+++ Terraform-Labs/storage-account.tf
@@ -1,11 +1,11 @@
-resource "random_id" "randomID" {
+resource "random_id" "randomid" {
   keepers = {
     resource_group = azurerm_resource_group.FirstResourceGroup.name
   }
   byte_length = 8
 }
 resource "azurerm_storage_account" "storageAccount" {
-  name                     = "diag${random_id.randomID.id}"
+  name                     = "diag${random_id.randomid.id}"
   resource_group_name      = azurerm_resource_group.FirstResourceGroup.name
   location                 = var.location
   account_tier             = "Standard"
