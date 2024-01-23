675143d31517f39b15900700426d8554d1f3d65a
shreyas186 <shreyas.acharya.186@gmail.com>
2024-01-23 14:14:10 +0530

    Adding 3 VM's in a single subnet

--- Terraform-Labs/nic.tf
+++ Terraform-Labs/nic.tf
@@ -12,6 +12,7 @@ resource "azurerm_network_interface" "myNIC" {
   }
 }
 resource "azurerm_network_interface_security_group_association" "myNICSecurityGroupAssociation" {
+  count               = length(var.nicname)
   network_interface_id          = azurerm_network_interface.myNIC[count.index].id
   network_security_group_id = azurerm_network_security_group.myNSG.id
 }
