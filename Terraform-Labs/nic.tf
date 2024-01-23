1ca763bfce866f46961fe870d0fec24d8ef717a4
shreyas186 <shreyas.acharya.186@gmail.com>
2024-01-23 14:43:56 +0530

    Adding 3 VM's in a single subnet

--- Terraform-Labs/nic.tf
+++ Terraform-Labs/nic.tf
@@ -8,11 +8,18 @@ resource "azurerm_network_interface" "myNIC" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.myTerraformSubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.example[count.index].id
+    public_ip_address_id = azurerm_lb.example.frontend_ip_configuration[0].public_ip_address_id
   }
 }
 resource "azurerm_network_interface_security_group_association" "myNICSecurityGroupAssociation" {
   count               = length(var.nicname)
   network_interface_id          = azurerm_network_interface.myNIC[count.index].id
   network_security_group_id = azurerm_network_security_group.myNSG.id
+}
+resource "azurerm_network_interface_backend_address_pool_association" "example" {
+  count                        = length(var.nicname)
+  network_interface_id         = azurerm_network_interface.myNIC[count.index].id
+  ip_configuration_name        = azurerm_network_interface.myNIC[count.index].ip_configuration[0].name
+  backend_address_pool_id      = azurerm_lb_backend_address_pool.example.id
+  # loadbalancer_id              = azurerm_lb.example.id
 }
--- Terraform-Labs/public-ip.tf
+++ Terraform-Labs/public-ip.tf
@@ -1,9 +1,25 @@
 resource "azurerm_public_ip" "example" {
   count = length(var.ipaddress)
-  name                = var.ipaddress[tostring(count.index)]
+  name                = var.ipaddress
   resource_group_name = azurerm_resource_group.FirstResourceGroup.name
   location            = var.location
   allocation_method   = "Dynamic"
 
   tags = var.tags
+}
+resource "azurerm_lb" "example" {
+  name                = "myLoadBalancer"
+  resource_group_name = azurerm_resource_group.FirstResourceGroup.name
+  location            = var.location
+
+  frontend_ip_configuration {
+    name                 = "myFrontendIP"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "example" {
+  name                = "myBackendPool"
+  # resource_group_name = azurerm_resource_group.FirstResourceGroup.name
+  loadbalancer_id     = azurerm_lb.example.id
 }
