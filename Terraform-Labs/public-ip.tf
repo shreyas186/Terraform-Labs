603afef119c88da13924959a4d23781412ddda93
shreyas186 <shreyas.acharya.186@gmail.com>
2024-01-23 14:25:37 +0530

    Adding 3 VM's in a single subnet

--- Terraform-Labs/public-ip.tf
+++ Terraform-Labs/public-ip.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "example" {
   count = length(var.ipaddress)
-  name                = var.ipaddress[count.index]
+  name                = var.ipaddress[string(count.index)]
   resource_group_name = azurerm_resource_group.FirstResourceGroup.name
   location            = var.location
   allocation_method   = "Dynamic"
